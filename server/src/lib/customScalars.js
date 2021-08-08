d12ec32626bbb84efba90aa9e92354919745fe23
Steve <heystevegray.dev@gmail.com>
2021-08-07 23:58:44 -0400

    Added a DateTimeResolver and documentation.

---
+++ server/src/lib/customScalars.js
@@ -0,0 +1,31 @@
+import { ApolloError } from "apollo-server";
+import { GraphQLScalarType, parseValue } from "graphql";
+import { isISO8601 } from "validator";
+
+export const DateTimeResolver = new GraphQLScalarType({
+	name: "DateTime",
+	description: "And ISO 8601-encoded UTC date string.",
+	parseValue: (value) => {
+		if (isISO8601(value)) {
+			return value;
+		}
+		throw new ApolloError("DateTime must be a valid ISO 8601 Date string");
+	},
+	serialize: (value) => {
+		if (typeof value !== "string") {
+			value = value.toString();
+		}
+
+		if (isISO8601(value)) {
+			return value;
+		}
+
+		throw new ApolloError("DateTime must be a valid ISO 8601 Date string");
+	},
+	parseLiteral: (ast) => {
+		if (isISO8601(ast.value)) {
+			return ast.value;
+		}
+		throw new ApolloError("DateTime must be a valid ISO 8601 Date String");
+	},
+});
