38bd8664eed7498c15c9012f198fe6fe39d9448d
Steve <heystevegray.dev@gmail.com>
2021-08-08 00:44:55 -0400

    Added an AccountsDataSource.

--- server/src/services/accounts/resolvers.js
+++ server/src/services/accounts/resolvers.js
@@ -1,14 +1,11 @@
 import { UserInputError } from "apollo-server";
-
 import { DateTimeResolver } from "../../lib/customScalars";
-import auth0 from "../../config/auth0";
-import getToken from "../../lib/getToken";
 
 const resolvers = {
 	DateTime: DateTimeResolver,
 	Account: {
-		__resolveReference(reference, context, info) {
-			return auth0.getUser({ id: reference.id });
+		__resolveReference(reference, { dataSources }, info) {
+			return dataSources.accountsAPI.getAccountById(reference.id);
 		},
 		id(account, args, context, info) {
 			return account.user_id;
@@ -28,130 +25,49 @@ const resolvers = {
 		},
 	},
 	Query: {
-		account(parent, { id }, context, info) {
-			return auth0.getUser({ id });
+		account(parent, { id }, { dataSources }, info) {
+			return dataSources.accountsAPI.getAccountById(id);
 		},
-		accounts(parent, { id }, context, info) {
-			return auth0.getUsers();
+		accounts(parent, { id }, { dataSources }, info) {
+			return dataSources.accountsAPI.getAccounts();
 		},
-		viewer(parent, args, { user }, info) {
+		viewer(parent, args, { user, dataSources }, info) {
 			if (user && user.sub) {
-				return auth0.getUser({ id: user.sub });
+				return dataSources.accountsAPI.getAccountById(user.sub);
 			}
 			return null;
 		},
 	},
 	Mutation: {
-		createAccount(parent, { data: { email, password } }, context, info) {
-			return auth0.createUser({
-				app_metadata: {
-					groups: [],
-					roles: ["author"],
-					permissions: [
-						"read:own_account",
-						"edit:own_account",
-						"read:any_profile",
-						"edit:own_profile",
-						"read:any_content",
-						"edit:own_content",
-						"upload:own_media",
-					],
-				},
-				connection: "Username-Password-Authentication",
-				email,
-				password,
-			});
-		},
-		async deleteAccount(parent, { where: { id } }, context, info) {
-			await auth0.deleteUser({ id });
-			return true;
-		},
-		async changeAccountModeratorRole(
+		changeAccountModeratorRole(
 			parent,
 			{ where: { id } },
-			context,
+			{ dataSources },
 			info
 		) {
-			const authorPermissions = [
-				"read:own_account",
-				"edit:own_account",
-				"read:any_profile",
-				"edit:own_profile",
-				"read:any_content",
-				"edit:own_content",
-				"upload:own_media",
-			];
-			const moderatorPermissions = [
-				"read:any_account",
-				"block:any_account",
-				"promote:any_account",
-				"block:any_content",
-			];
-
-			const user = await auth0.getUser({ id });
-			const isModerator = user.app_metadata.roles.includes("moderator");
-			const roles = isModerator ? ["author"] : ["moderator"];
-			const permissions = isModerator
-				? authorPermissions
-				: authorPermissions.concat(moderatorPermissions);
-
-			return auth0.updateUser(
-				{ id },
-				{ app_metadata: { groups: [], roles, permissions } }
-			);
+			return dataSources.accountsAPI.changeAccountModeratorRole(id);
 		},
-		async changeAccountBlockedStatus(
+		changeAccountBlockedStatus(
 			parent,
 			{ where: { id } },
-			context,
+			{ dataSources },
 			info
 		) {
-			const { blocked } = await auth0.getUser({ id });
-			return auth0.updateUser({ id }, { blocked: !blocked });
+			return dataSources.accountsAPI.changeAccountBlockedStatus(id);
 		},
-		async updateAccount(
+		createAccount(
 			parent,
-			{ data: { email, newPassword, password }, where: { id } },
-			context,
+			{ data: { email, password } },
+			{ dataSources },
 			info
 		) {
-			// Handle user input-related errors:
-			// -> Throw and error if no email or password was submitted
-			// -> Throw and error if both an email or password was submitted
-			// -> Throw and error if an existing password was submitted without a new password, or a new password was submitted without an existing password
-			// If no email was submitted, we know we're updating the password:
-			// -> Make a request to Auth0 to ensure we can authenticate the user with the existing password they submitted
-			// -> Call Auth0's update user method to update the password
-			// We know we have an email, so update it:
-			// -> Call Auth0s's update user method to update the email
-			if (!email && !newPassword && !password) {
-				throw new UserInputError(
-					"You must supply some account data to update."
-				);
-			} else if (email && newPassword && password) {
-				throw new UserInputError(
-					"Email and password cannot be updated simultaneously."
-				);
-			} else if (
-				(!password && newPassword) ||
-				(password && !newPassword)
-			) {
-				throw new UserInputError(
-					"Provide the existing and new passwords when updating the password"
-				);
-			}
-			if (!email) {
-				const user = await auth0.getUser({ id });
-				await getToken(user.email, password);
-				return auth0.updateUser({ id }, { password: newPassword });
-			}
-
-			return auth0.updateUser(
-				{ id },
-				{
-					email,
-				}
-			);
+			return dataSources.accountsAPI.createAccount(email, password);
+		},
+		deleteAccount(parent, { where: { id } }, { dataSources }, info) {
+			return dataSources.accountsAPI.deleteAccount(id);
+		},
+		updateAccount(parent, { data, where: { id } }, { dataSources }, info) {
+			return dataSources.accountsAPI.updateAccount(id, data);
 		},
 	},
 };
