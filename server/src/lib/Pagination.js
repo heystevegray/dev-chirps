3cacf6b0425a919337c513124e5ce8a82f41ff23
Steve <heystevegray@gmail.com>
2021-08-15 21:12:42 -0400

    Getting pageInfo

--- server/src/lib/Pagination.js
+++ server/src/lib/Pagination.js
@@ -21,7 +21,7 @@ class Pagination {
 			// forward pagination happens here
 			const operator = this._getOperator(sort);
 			const queryDoc = after
-				? await this._getFilterWithCursor(after, filter, operator)
+				? await this._getFilterWithCursor(after, filter, operator, sort)
 				: filter;
 
 			const docs = await this.Model.find(queryDoc)
@@ -33,7 +33,25 @@ class Pagination {
 				: [];
 		} else {
 			// backward pagination happens here
-			// edges = ???
+			const reverseSort = this._reverseSortDirection(sort);
+			const operator = this._getOperator(reverseSort);
+			const queryDoc = before
+				? await this._getFilterWithCursor(
+						before,
+						filter,
+						operator,
+						reverseSort
+				  )
+				: filter;
+
+			const docs = await this.Model.find(queryDoc)
+				.sort(reverseSort)
+				.limit(last)
+				.exec();
+
+			edges = docs.length
+				? docs.map((doc) => ({ cursor: doc._id, node: doc })).reverse()
+				: [];
 		}
 
 		// Handle user input errors
@@ -60,6 +78,29 @@ class Pagination {
 
 	// Get pagination information
 	async getPageInfo(edges, queryArgs) {
+		if (edges.length) {
+			const { filter = {}, sort = {} } = queryArgs;
+			const startCursor = this._getStartCursor(edges);
+			const endCursor = this._getEndCursor(edges);
+			const hasNextPage = await this._getHasNextPage(
+				endCursor,
+				filter,
+				sort
+			);
+			const hasPreviousPage = await this._getHasPreviousPage(
+				startCursor,
+				filter,
+				sort
+			);
+
+			return {
+				hasNextPage,
+				hasPreviousPage,
+				startCursor,
+				endCursor,
+			};
+		}
+
 		return {
 			hasNextPage: false,
 			hasPreviousPage: false,
@@ -91,16 +132,26 @@ class Pagination {
 				`No record found for ID '${fromCursorId}'`
 			);
 		}
+
 		filterWithCursor.$and.push({ [field]: { [operator]: fromDoc[field] } });
 
 		return filterWithCursor;
 	}
 
 	// Create the aggregation pipeline to paginate a full text-search
-	async _getSearchPipeline(fromCursorId, filter, first, operator, sort) { }
+	async _getSearchPipeline(fromCursorId, filter, first, operator, sort) {}
 
 	// Reverse the sort direction when queries need to look in the opposite direction of the set sore order (e.g. next/previous page checks)
-	_reverseSortDirection(sort) { }
+	_reverseSortDirection(sort) {
+		const fieldArray = Object.keys(sort);
+		if (fieldArray.length === 0) {
+			// Sort by the $natural sort order by default
+			return { $natural: -1 };
+		}
+
+		const field = fieldArray[0];
+		return { [field]: sort[field] * -1 };
+	}
 
 	// Get the correct comparison operator based on the sort order
 	_getOperator(sort, options = {}) {
@@ -109,21 +160,63 @@ class Pagination {
 	}
 
 	/// Determine if a query is a full-text search based on the sort expression
-	_isSearchQuery(sort) { }
+	_isSearchQuery(sort) {}
 
 	// Check if a next page of results is available
-	async _getHasNextPage(endCursor, filter, sort) { }
+	async _getHasNextPage(endCursor, filter, sort) {
+		const operator = this._getOperator(sort);
+		const queryDoc = await this._getFilterWithCursor(
+			endCursor,
+			filter,
+			operator,
+			sort
+		);
+		const nextPage = await this.Model.findOne(queryDoc)
+			.select("_id")
+			.sort(sort);
+
+		return Boolean(nextPage);
+	}
 
 	// Check if a previous page of results is available
-	async _getHasPreviousPage(startCursor, filter, sort) { }
+	async _getHasPreviousPage(startCursor, filter, sort) {
+		const reverseSort = this._reverseSortDirection(sort);
+		const operator = this._getOperator(reverseSort);
+		const queryDoc = await this._getFilterWithCursor(
+			startCursor,
+			filter,
+			operator,
+			reverseSort
+		);
+
+		const previousPage = await this.Model.findOne(queryDoc)
+			.select("_id")
+			.sort(reverseSort);
+
+		return Boolean(previousPage);
+	}
 
 	// Get the ID of the first document in the paging window
-	_getStartCursor(edges) { }
+	_getStartCursor(edges) {
+		if (!edges.length) {
+			return null;
+		}
+
+		return edges[0].cursor;
+	}
 
 	// Get the ID of the last document in the paging window
-	_getEndCursor(edges) { }
+	_getEndCursor(edges) {
+		if (!edges.length) {
+			return null;
+		}
+
+		return edges[edges.length - 1].cursor;
+	}
 
 	_throwError(message) {
 		throw new UserInputError(message);
 	}
 }
+
+export default Pagination;
--- server/src/services/profiles/resolvers.js
+++ server/src/services/profiles/resolvers.js
@@ -18,9 +18,10 @@ const resolvers = {
 			);
 		},
 		following(profile, args, { dataSources }, info) {
-			return dataSources.profilesAPI.getFollowedProfiles(
-				profile.following
-			);
+			return dataSources.profilesAPI.getFollowedProfiles({
+				...args,
+				following: profile.following,
+			});
 		},
 	},
 	Account: {
