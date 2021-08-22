60c956b4557614361e0a517c59e5b17c46d96010
Steve <heystevegray@gmail.com>
2021-08-22 14:31:29 -0400

    Setup Grommet

---
+++ client/src/styles/global.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export default createGlobalStyle`
+${reset}
+
+* {
+	box-sizing: border-box
+}
+
+body {
+	background: #202020;
+	color: #fff;
+	min-height: 100vh
+}
+
+a {
+	text-decoration: none;
+}
+
+img {
+	height: auto;
+	max-width: 100%;
+}
+`;
