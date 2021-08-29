bce62dec48065540915cd0626e900ff091e6755a
Steve <heystevegray@gmail.com>
2021-08-29 17:10:02 -0400

    Fixed theme colors for tabs

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -1,6 +1,17 @@
 import { ThemeType } from "grommet";
 
 const theme: ThemeType = {
+	tab: {
+		color: "red",
+		hover: {
+			color: "dark-3",
+		},
+		border: {
+			hover: { color: "dark-3" },
+			active: { color: "#32a852" },
+			color: "brand",
+		},
+	},
 	global: {
 		font: {
 			family: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
@@ -14,6 +25,10 @@ const theme: ThemeType = {
 				light: "#ff4141",
 			},
 			"dark-3": "#bababa",
+		},
+		focus: {
+			outline: { color: "#32a852" },
+			border: { color: "#32a852" },
 		},
 	},
 };
