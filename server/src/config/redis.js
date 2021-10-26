ec15dfbeb3e49969ed44d59f013ca5e79482f78d
Steve <heystevegray@gmail.com>
2021-10-25 20:56:06 -0400

    Configure Docker for Production

--- server/src/config/redis.js
+++ server/src/config/redis.js
@@ -4,7 +4,13 @@ import RedisSMQ from "rsmq";
 const host = process.env.REDIS_HOST_ADDRESS;
 const port = Number(process.env.REDIS_PORT);
 
-const client = redis.createClient({ host, port });
+const client = redis.createClient({
+	...(process.env.NODE_ENV === "production" && {
+		password: process.env.REDIS_PASSWORD,
+	}),
+	host,
+	port,
+});
 
 client.on("Connect", () => {
 	console.log(`Redis connection is ready | http://${host}:${port}/`);
