bfd44b20a03016d3946f24ed5c7b2aa30bef97ed
Steve <heystevegray@gmail.com>
2021-10-30 01:15:35 -0400

    Hide full name in CreateProfileForm

--- client/src/components/Forms/CreateProfileForm/index.tsx
+++ client/src/components/Forms/CreateProfileForm/index.tsx
@@ -1,5 +1,5 @@
 import { useMutation } from "@apollo/client";
-import { Form, FormField } from "grommet";
+import { Form, FormField, Box, CheckBox } from "grommet";
 import { useState } from "react";
 import { CREATE_PROFILE } from "../../../graphql/mutations";
 import { GET_VIEWER } from "../../../graphql/queries";
@@ -11,6 +11,7 @@ import {
 	validateDescription,
 	validateUsername,
 } from "../EditProfileForm";
+import FormFieldContainer from "../FormFieldContainer";
 
 interface Props {
 	accountId: string;
@@ -19,6 +20,7 @@ interface Props {
 
 const CreateProfileForm = ({ accountId, updateViewer }: Props) => {
 	const [descCharCount, setDescCharCount] = useState(0);
+	const [isFullNameHidden, setIsFullNameHidden] = useState(false);
 	const [createProfile, { error: createProfileError, loading }] = useMutation(
 		CREATE_PROFILE,
 		{
@@ -46,6 +48,7 @@ const CreateProfileForm = ({ accountId, updateViewer }: Props) => {
 					variables: {
 						data: {
 							accountId,
+							isFullNameHidden,
 							...event.value,
 						},
 					},
@@ -54,43 +57,64 @@ const CreateProfileForm = ({ accountId, updateViewer }: Props) => {
 				});
 			}}
 		>
-			<FormField
-				htmlFor="username"
-				id="username"
-				label={<RequiredLabel>Username</RequiredLabel>}
-				name="username"
-				required
-				placeholder="Pick a unique username"
-				validate={(fieldData) => validateUsername(fieldData)}
-			/>
-
-			<FormField
-				htmlFor="fullName"
-				id="fullName"
-				label="Full Name"
-				name="fullName"
-				placeholder="Add your full name"
-			/>
-
-			<FormField
-				htmlFor="description"
-				id="description"
-				label={
-					<CharacterCountLabel
-						currentCharacters={descCharCount}
-						label="Description"
-						max={256}
-					/>
+			<FormFieldContainer
+				button={
+					<LoadingButton loading={loading} label="Save Profile" />
 				}
-				name="description"
-				placeholder="Write a short bio or description about yourself"
-				onInput={(event: any) => {
-					const characterCount = event.target.value.length;
-					setDescCharCount(characterCount);
-				}}
-				validate={(fieldData) => validateDescription(fieldData)}
-			/>
-			<LoadingButton loading={loading} label="Save Profile" />
+			>
+				<FormField
+					htmlFor="username"
+					id="username"
+					label={<RequiredLabel>Username</RequiredLabel>}
+					name="username"
+					required
+					placeholder="Pick a unique username"
+					validate={(fieldData) => validateUsername(fieldData)}
+				/>
+
+				<FormField
+					htmlFor="fullName"
+					id="fullName"
+					label="Full Name"
+					name="fullName"
+					placeholder="Add your full name"
+				/>
+
+				<FormField
+					htmlFor="hideFullName"
+					id="hideFullName"
+					name="hideFullName"
+				>
+					<Box pad={{ bottom: "medium", top: "medium" }}>
+						<CheckBox
+							checked={isFullNameHidden}
+							label="Hide your full name from squeaken"
+							onChange={(event) => {
+								setIsFullNameHidden(event.target.checked);
+							}}
+						/>
+					</Box>
+				</FormField>
+
+				<FormField
+					htmlFor="description"
+					id="description"
+					label={
+						<CharacterCountLabel
+							currentCharacters={descCharCount}
+							label="Description"
+							max={256}
+						/>
+					}
+					name="description"
+					placeholder="Write a short bio or description about yourself"
+					onInput={(event: any) => {
+						const characterCount = event.target.value.length;
+						setDescCharCount(characterCount);
+					}}
+					validate={(fieldData) => validateDescription(fieldData)}
+				/>
+			</FormFieldContainer>
 		</Form>
 	);
 };
